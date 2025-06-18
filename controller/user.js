a1601592a4a09f0b0c963094c85ebaab1ee410e7
pratikparmar435 <prateekparmar435@gmial.com>
2025-06-18 23:12:50 +0530

    Sign-up route completed

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,14 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //sign-up route logic
 let createUser = async (req, res) => {
   let { username, email, password } = req.body;
-  console.log(username, email, password);
+  let hashedPassword = await bcrypt.hash(password, 12);
+  let newUser = new User({ username, email, password: hashedPassword });
+  await newUser.save(newUser);
+  req.flash("success", "You are registered!");
+  res.redirect("/login");
 };
 
 module.exports = { createUser };
